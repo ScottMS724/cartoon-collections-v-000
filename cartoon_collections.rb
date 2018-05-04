26ccbdcb322a08afdfc0de03e766bf934a4d5392
Scott Silverman <scottms724@gmail.com>
2018-05-04 16:43:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if (array & cheese_types).join == nil 
+  if (array & cheese_types).join
     return (array & cheese_types.join)
   else 
     return (array & cheese_types).join
