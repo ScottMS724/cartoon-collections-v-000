64e5b35068d58413517e6241419b3b4384b5314b
Scott Silverman <scottms724@gmail.com>
2018-05-04 16:23:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def long_planeteer_calls(callsArray)
   callsArray.any? { |i| i.length > 4 }
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
